cdbfe1b6120c88f87e81f002518e10531532a2d2
zabokaa <>
2023-06-22 21:44:40 +0200

    Added style movieView

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,27 +1,30 @@
 import PropTypes from "prop-types";
 import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container"
 
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
+    <Container>
 
-    <Card
-      onClick={() =>
-        onMovieClick(movie)
-      }
-    >
-      <Card.Img
-        variant="top"
-        src={movie.image}
-        style={{ width: "200px", height: "300px" }}
-        rounded
-      />
-      <Card.Body>
-        <Card.Title>{movie.title}</Card.Title>
-        <Card.Text>{movie.year}</Card.Text>
-      </Card.Body>
-    </Card>
+      <Card
+        onClick={() =>
+          onMovieClick(movie)
+        }
+      >
+        <Card.Img
+          variant="top"
+          src={movie.image}
+          style={{ width: "200px", height: "300px" }}
+          rounded
+        />
+        <Card.Body>
+          <Card.Title>{movie.title}</Card.Title>
+          <Card.Text>{movie.year}</Card.Text>
+        </Card.Body>
+      </Card>
 
+    </Container>
 
   );
 };
